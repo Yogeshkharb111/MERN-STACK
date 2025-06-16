0099783ac306935d6b199e3b5d4f960ea0aa70ef
Yogeshkharb111 <yogeshkharb111@gmail.com>
2025-06-16 13:35:47 +0530

    day1-pract
    description

---
+++ Day1/app.js
@@ -0,0 +1,14 @@
+window.console.log("Hello from JS!");
+
+// let age=10;
+// const year=2010;
+// console.log(year)
+// var block =22;
+// console.log(block)
+// console.log(age,year,block)
+
+console.log(age)
+var age =22;
+console.log(age)
+var age=44;
+console.log(age)

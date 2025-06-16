c7f081aad97e7c1d71d2e2cdfbda11c31b161572
Yogeshkharb111 <yogeshkharb111@gmail.com>
2025-06-16 14:47:46 +0530

    updating code

--- Day1/part2/app.js
+++ Day1/part2/app.js
@@ -1,31 +1,31 @@
 window.console.log("Hello from part2 of JS");
 
-let juice=50;
-let gstOnJuice=0.1;
-let fries=70;
-let gstOnFood=0.05;
+let juice = 50;
+let gstOnJuice = 0.1;
+let fries = 70;
+let gstOnFood = 0.05;
 
-let juiceBillAmount=juice+juice+gstOnJuice;
-let friesBillAmount=fries+fries+gstOnFood;
-const finalBill=juiceBillAmount+friesBillAmount;
-console.log("finalBill:",finalBill)
+let juiceBillAmount = juice + juice * gstOnJuice;  
+let friesBillAmount = fries + fries * gstOnFood;   
+const finalBill = juiceBillAmount + friesBillAmount;
+console.log("finalBill:", finalBill);
 
-
-function calculateJuiceBillAmount(juice,gstOnJuice) {
-    //let gstOnJuice=0.1
-    let juiceBillAmount=juice+juice+gstOnJuice;
-    console.log(juiceBillAmount)
+function printBill(name, amount) {
+    console.log("-------------");
+    console.log("Your Bill for", name);
+    console.log(" RS.", amount);
+    console.log("-------------");
 }
 
-calculateJuiceBillAmount(60,0.1);
-calculateJuiceBillAmount(60,0.2);
-calculateJuiceBillAmount(60,0.3);
+function calculateBillAmount(price, gst) {
+    console.log("----Calculating----");
+    let billAmount = price + price * gst;  
+    return billAmount;
+}
 
-
-
-
-// function calculateFriesBillAmount() {
-//     let fries=50;
-//     let friesBillAmount=fries+fries+gstOnfries;
-//     console.log(friesBillAmount)
-// }
+let res1 = calculateBillAmount(60, 0.1);
+printBill("Mango Juice", res1);
+let res2 = calculateBillAmount(60, 0.1);
+printBill("Mango Juice", res2);
+let res3 = calculateBillAmount(60, 0.2);
+printBill("Mango Juice", res3);
